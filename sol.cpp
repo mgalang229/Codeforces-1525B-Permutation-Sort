cea230705afca25cefa5efe018f9043d34bfc0a3
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-06-09 11:18:13 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n;
+		cin >> n;
+		vector<int> a(n);
+		for (int i = 0; i < n; i++) {
+			cin >> a[i];
+		}
+		vector<int> b = a;
+		sort(b.begin(), b.end());
+		bool checker = true;
+		for (int i = 0; i < n; i++) {
+			if (a[i] != b[i]) {
+				// check if the permutation is already sorted
+				checker = false;
+				break;
+			}
+		}
+		if (checker) {
+			// if it's already sorted, then output 0
+			cout << 0;
+		} else if (a[0] == b[0] && a[n - 1] == b[n - 1]) {
+			// if the first AND last elements are in place, then output 1
+			cout << 1;
+		} else if ((a[0] == b[0]) || (a[n - 1] == b[n - 1])) {
+			// if the first OR last element is in place, then output 1
+			cout << 1;
+		} else if (a[0] == b[n - 1] && a[n - 1] == b[0]) {
+			// if the first and last elements switched places, then output 3
+			cout << 3;
+		} else {
+			// otherwise, the minimum number of operations will always be 2
+			cout << 2;
+		}
+		cout << '\n';
+	}
+	return 0;
+}
